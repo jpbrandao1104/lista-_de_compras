b9cc510f72dd9b48a4b50bb33a5205a9452aa2ae
Joao Pedro <jpbrandao1104@gmail.com>
2026-06-28 14:22:00 -0300

    Projeto final

--- App.java
+++ App.java
@@ -1,43 +1,142 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-public class App{
-    public static void main(String[] args) throws Exception {
-        Scanner leia = new Scanner(System.in);
-        ArrayList<String> listaCompras = new ArrayList<>();
-        int opcaoLista = 0;
-        int opcao = 0;
-        do{
-            //trtert
-            try{
-            System.out.println("""
-            Escolha uma opcao
 
-            1-Criar uma nova lista de compras
-            2-Sair
+import java.util.ArrayList;
+public class App{
+    public static void main(String[]args)throws Exception{
+      
+        
+
+         ArrayList<String> MinhaLista = new ArrayList<String>();
+        Scanner entrada = new Scanner(System.in);
+        int CriarLista =0;
+        boolean ListaCriada = false;
+        
+        
+
+        do{
+             try{
+            
+        System.out.println("Bem vindo a Loja do Zé ");
+        System.out.println("""
+
+                    Escolha uma opção
+
+                    1- Criar Lista
+                    2- Sair       
 
                 """);
-              opcaoLista = leia.nextInt();
-                leia.nextLine();
+            
+            CriarLista = entrada.nextInt();
+            entrada.nextLine();
 
-                switch (opcaoLista) {
-                    case 1 -> {System.out.println("Lista criada");
-
-                    do{
-                        System.out.println("""
-                                EScolha a opcao desejada:
-                                1-Adicionar um item a lista \n
-                                """);
-
-                    }while(opcao != 5);
+            switch(CriarLista){
+                case 1 ->{System.out.println("sua Lista foi criada");
+                            System.out.println("                     ");
+                    ListaCriada = true;
                 }
-                    case 2 -> System.out.println("Obrigado por usar o nosso sistema");
-                    default -> System.out.println("opcao invalida");
+                case 2 ->{System.out.println("Obrigado por usar o nosso sistema");
+                    return;
                 }
-        }catch(Exception erro){
-            System.out.println("Erro!!! entrada invalida");
-            leia.nextLine();
-            opcaoLista = 1;
-        }
-            }while(opcaoLista != 2);
+                
+            }
+            //nao mexer, nao sei pq funciona(pesquisar depois)
+            }catch(Exception Erro){
+            System.out.println("Erro tentar novamente");
+            entrada.nextLine();
+            CriarLista = 0;
+           }
+        }while(CriarLista != 1);
+           
+            
+           
+
+
+          int Lista = 0;
+            //colocar doWhile aqui
+            do {
+                try {
+                    //colocar trycatch aqui
+                    System.out.println("""
+                                    O que deseja?
+                                    
+                                    1- Adicionar item na lista
+                                    2- ver lista
+                                    3- Remover item da Lista
+                                    4- Limpar lista
+                                    5- Sair
+                                    
+
+                            """);
+
+                    Lista = entrada.nextInt();
+                    entrada.nextLine();
+
+                    switch (Lista) {
+                        case 1 -> {
+                            String AdicionarItem = "";
+                            System.out.println("Item desejado:");
+                            System.out.println("digite o item");
+                            AdicionarItem = entrada.nextLine();
+                            MinhaLista.add(AdicionarItem);
+                        }
+
+                        case 2 -> {
+                            if (MinhaLista.isEmpty()) {
+                                System.out.println("A lista está vazia!");
+                            } else {
+                                System.out.println("Itens da Lista ");
+                                for (int i = 0; i < MinhaLista.size(); i++) {
+                                    System.out.println("\n" + (i + 1) + " - " + MinhaLista.get(i));
+                                }
+                                System.out.println("");
+                            }
+                        }
+
+                        case 3 -> {
+                            if (MinhaLista.isEmpty()) {
+                                System.out.println("A lista está vazia, nada para remover!");
+                            } else {
+                                System.out.println("Itens da Lista ");
+                                for (int i = 0; i < MinhaLista.size(); i++) {
+                                    System.out.println((i + 1) + " - " + MinhaLista.get(i));
+                                }
+                                System.out.println("");
+                                System.out.println("Digite o número do item para remover:");
+                                int numRemover = entrada.nextInt();
+                                entrada.nextLine();
+                                if (numRemover >= 1 && numRemover <= MinhaLista.size()) {
+                                    String itemRemovido = MinhaLista.remove(numRemover - 1);
+                                    System.out.println("Item  " + itemRemovido +" removido com sucesso!");
+                                } else {
+                                    System.out.println("Número inválido, tente novamente.");
+                                }
+                            }
+                        }
+                        case 4 -> {
+                             if (MinhaLista.isEmpty()) {
+                             System.out.println("A lista já está vazia!");
+                            } else {
+                             MinhaLista.clear();
+                             System.out.println("Todos os itens foram removidos da lista!");
+                            }
+                        }
+                        case 5 -> {
+                            System.out.println("obrigado por usar a nossa loja");
+                        }
+                    }
+
+                } catch (Exception Erro2) {
+                    System.out.println("Erro tentar novamente");
+                    entrada.nextLine();
+                    Lista = 0;
+                }
+            } while (Lista != 5);
+                            
+                    entrada.close();
     }
-}
+}        
+        
+        
+    
+
+    
